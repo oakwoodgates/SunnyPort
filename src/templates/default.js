bfea7080a6aa239116adbd3a9cbb2b6eadf173e8
WPGuru4u <wpguru4u@gmail.com>
2020-11-16 16:31:48 -0500

    Better date format

--- src/templates/default.js
+++ src/templates/default.js
@@ -29,12 +29,12 @@ function opContactPurchaseTable ( args ) {
   if ( args.ontraport.id ) {
     if ( args.ontraport.purchases ) {
       return `
-        <p class="meta text-center">Will display up to the 50 most recent purchases. Dates displayed as MM/DD/YYYY</p>
+        <p class="meta text-center">Will display up to the 50 most recent purchases. Dates displayed as M/D/YY</p>
         <table class="table-striped">
           <tr><td></td><td></td><td></td></tr>
           ${args.ontraport.purchases.map((item, i) => `
             <tr>
-              <td class="meta">${new Date(item.date * 1000).toLocaleDateString("en-US")}</td>
+              <td class="meta">${new Date(item.date * 1000).toLocaleDateString("en-US", {day:'numeric',month:'numeric',year:'2-digit'})}</td>
               <td class="meta">$${item.total_price}</td>
               <td class="fs-small">${truncateString(item.name,45)}</td>
             </tr>
