6f8b5b5dfbaba4ccc94dc47b8883ae94722777e1
WPGuru4u <wpguru4u@gmail.com>
2020-11-17 10:33:56 -0500

    Fix components imports; Add Subscriptions table

--- src/templates/default.js
+++ src/templates/default.js
@@ -1,6 +1,7 @@
-import { opContactTagsTable } from '../components/.js'
-import { opContactDataTable } from '../components/.js'
-import { opContactPurchaseTable } from '../components/.js'
+import opContactTagsTable from '../components/opContactTagsTable.js'
+import opContactDataTable from '../components/opContactDataTable.js'
+import opContactPurchaseTable from '../components/opContactPurchaseTable.js'
+import opContactSubscriptionTable from '../components/opContactSubscriptionTable.js'
 import I18n from '../javascripts/lib/i18n.js'
 
   export default function (args) {
@@ -17,6 +18,10 @@ import I18n from '../javascripts/lib/i18n.js'
         <button class="accordion">Purchase History</button>
         <div class="panel">
           ${opContactPurchaseTable(args)}
+        </div>
+        <button class="accordion">Subscriptions</button>
+        <div class="panel">
+          ${opContactSubscriptionTable(args)}
         </div>
         <script>
           var acc = document.getElementsByClassName("accordion");
