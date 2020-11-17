8eb30eb937ecdd1fc491d7c59e515b0cc42962b3
WPGuru4u <wpguru4u@gmail.com>
2020-11-16 19:32:22 -0500

    Purchase status colors

--- src/templates/default.js
+++ src/templates/default.js
@@ -34,9 +34,10 @@ function opContactPurchaseTable ( args ) {
           <tr><td></td><td></td><td></td></tr>
           ${args.ontraport.purchases.map((item, i) => `
             <tr>
+              <td><span class="purchase-status purchase-status-${item.status}"></span></td>
+              <td class="fs-small">${truncateString(item.name,45)}</td>
               <td class="meta">${new Date(item.date * 1000).toLocaleDateString("en-US", {day:'numeric',month:'numeric',year:'2-digit'})}</td>
               <td class="meta">$${item.total_price}</td>
-              <td class="fs-small">${truncateString(item.name,45)}</td>
             </tr>
             `.trim()).join('')}
         </table>`
